8fbc96c565f8675a3b5017750b034f394d735198
Idar Ulvestad Haugstuen <idaruh@gmail.com>
2022-02-09 23:45:43 +0100

    New animations to the logo.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Logo from "./Logo";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Logo/>
+            </header>
+        </div>
+    );
 }
 
 export default App;
